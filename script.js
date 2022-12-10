1bd6939dc4269cdb69c7923fae6a5f6d2f85cb95
kubaglowczynski <114954433+kubaglowczynski@users.noreply.github.com>
2022-12-10 17:30:17 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+window.onload = function(){
+    fetch("https://randomuser.me/api")
+    .then(response => response.json())
+    .then(data => dataReady(data))
+}
+
+function dataReady(data){
+    var result = data.results[0];
+    var name = result.name.first;
+    document.querySelector(".content-top-profile-name").innerHTML =`Hi,${name}!`;
+    var image = result.picture.large;
+    document.querySelector(".content-top-profile img").src = image;
+    var username = result.login.username;
+    document.querySelector(".profile span").innerHTML = username;
+}
